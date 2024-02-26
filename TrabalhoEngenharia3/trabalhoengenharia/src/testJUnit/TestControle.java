653a12939ba95d2c03da39e66e5961877bba51dc
CarolAlvares23 <122477075+CarolAlvares23@users.noreply.github.com>
2024-02-26 10:50:59 -0300

    Update TestControle.java

--- TrabalhoEngenharia3/trabalhoengenharia/src/testJUnit/TestControle.java
+++ TrabalhoEngenharia3/trabalhoengenharia/src/testJUnit/TestControle.java
@@ -1,18 +1,1 @@
-package testJUnit;
 
-import projetoBiblioteca.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import java.lang.IllegalArgumentException;
-
-class TestControle {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
-}
